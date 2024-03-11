1bec6cf8e3a4934a697044a96c897b9c65882060
Ave <159603384+AveTavern@users.noreply.github.com>
2024-03-11 21:55:23 +0800

    Update yandex-ispmgr.py

--- yandex-ispmgr.py
+++ yandex-ispmgr.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime, timedelta 
 import time
 
-# Настройки времени
+# Time settings
 delta_from = 1
 delta_to = 1
 current_date = datetime.now()
@@ -19,17 +19,17 @@ date_to_str = str(date_to)
 date_to_req = ''+date_to_str+''
 #print("Date to "+date_to_req)
 
-# Импорт модуля работы с google sheets
+# Google sheet module import
 import gspread
 gs = gspread.service_account(filename='URL to service account file')  
 sh = gs.open_by_key('open key')
 
-# Импорт кред для доступа к Яндекс Метрики
+# Import of keys for YM
 API_token = 'OAuth AUTH'
 counter_id = 'ID'
 api_metrika_url = 'https://api-metrika.yandex.net/stat/v1/data/bytime'
 
-# Формирование запроса в Яндекс Метрику
+# Params for request to Yandex Metrics
 params = {
    'date1': date_from_req, 
    'date2': date_to_req, 
@@ -42,15 +42,15 @@ res = requests.get(api_metrika_url, params = params, headers={'Authorization': A
 #print(res.text)
 #print("")
 
-# Разбираем полученный запрос по составляющим для внедрения в таблицу.
-json_res = res.json()  # Превращение ответа в Python native object.
-data = json_res["data"] # Оставляем в ответе только данные, убирая информацию о запросе.
+# Decomposite request 
+json_res = res.json()  # Make request Python native object.
+data = json_res["data"] # Remove all unnesesary data
 #print(data)
 #print("")
 
-# Добавляем в техническую таблицу источники трафика
-a = 2 #строчка
-b = 1 #столбик
+# insert traffic sources into technical table
+a = 2 #row
+b = 1 #collumn
 for metrics in data: 
     dimensions = metrics["dimensions"]
     for id in dimensions:
@@ -60,7 +60,7 @@ for metrics in data:
         worksheet = worksheet.update_cell(a, b, name)
         a += 1
         
-# Добавляем в техническую таблицу значение трафика
+# insert traffic quantity into technical table
 a = 2
 for dimensions in data:
     metrics = dimensions["metrics"]
@@ -77,7 +77,7 @@ for dimensions in data:
         #print("last", a)
         #print("last", b)
         
-# Сравнение источников трафика между технической и актуальной таблицей        
+# Comparison of trafic sources from tech table and current        
 worksheet = sh.worksheet("current")
 gs_val_current = worksheet.col_values(1)
 gs_val_current.remove("Current month")
